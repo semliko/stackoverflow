cdaeed6f76f2e40dca0b22d209d63ed520a90b3f
semliko <megabytus@gmail.com>
2021-12-10 13:03:06 +0000

    fix asociations test

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  describe 'fields' do
-    it { should validate_presence_of :body }
-  end
 
   describe 'associations' do
     it { should belong_to(:question).class_name('Question') }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :body }
   end
 
 end
