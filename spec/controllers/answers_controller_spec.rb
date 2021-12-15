afe66cd8062217fc59363a60596b06e748b66a80
semliko <megabytus@gmail.com>
2021-12-15 20:55:49 +0000

    get method problem with routs

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -2,4 +2,20 @@ require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
 
+  let(:question) { create(:question) }
+  let(:answer) { create(:answer, question: question) }
+
+  describe 'GET #index' do
+    let(:answers) { create_list(:answer, 3, question: question) }
+
+    it 'populates an array of all answers' do
+      get question_answers_path(question)
+      expect(assigns(:answer)).to match_array(answers)
+    end
+
+    it 'renders index view' do
+      get :index
+      expect(response).to render_template :index
+    end
+  end
 end
