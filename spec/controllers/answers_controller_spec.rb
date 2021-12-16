21115bbda0690d6bc0495b3bc40515d0ec6b049b
semliko <megabytus@gmail.com>
2021-12-16 14:55:15 +0000

    create a new answer controller test

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -54,6 +54,17 @@ RSpec.describe AnswersController, type: :controller do
         expect(response).to redirect_to question_answer_path(question, assigns(:answer))
       end
     end
+
+    context 'with invalid attributes' do
+      it 'does not save the answer' do
+        expect { post :create, params: { question_id: question, answer: attributes_for(:answer, :invalid) } }.to_not change(question.answers, :count)
+      end
+
+      it 'redirects to show view' do
+        post :create, params: { question_id: question, answer: attributes_for(:answer, :invalid) }
+        expect(response).to render_template :new
+      end
+    end
   end
 
 end
