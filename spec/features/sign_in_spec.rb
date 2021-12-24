aadca6aae0da4394be09bff7c4f5ea6a0bcf1eaa
semliko <megabytus@gmail.com>
2021-12-24 14:05:21 +0000

    user login

---
+++ spec/features/sign_in_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+feature 'User can sign in', %(
+In order to ask questions
+As an anauthenticated user
+I'd like to be able to sign in) do
+
+  given(:user) { User.create!(email: 'user@test.com', password: '12345678') }
+
+  background do
+    visit new_user_session_path
+  end
+
+  scenario 'Registered tries to sign in' do
+    fill_in 'Email', with: user.email
+    fill_in "Password", with: user.password
+    click_on 'Log in'
+
+    #save_and_open_page
+    expect(page).to have_content 'Signed in successfully'
+  end
+  scenario 'Unregistered user tries to sign in' do
+    fill_in 'Email', with: 'wrong@test.com'
+    fill_in "Password", with: '12345678'
+    click_on 'Log in'
+
+    #save_and_open_page
+    expect(page).to have_content 'Invalid Email or password'
+  end
+end
