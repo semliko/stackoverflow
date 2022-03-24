aeb0357cddefcc6b750af44445101f89d9733199
semliko <megabytus@gmail.com>
2022-03-24 09:19:28 +0000

    add test

--- spec/features/answers/edit_spec.rb
+++ spec/features/answers/edit_spec.rb
@@ -33,6 +33,16 @@ feature 'User can edit his answer', %q{
       end
     end
 
+    scenario 'answer with file', js: true do
+      sign_in user
+      visit question_path(question)
+      fill_in 'answer_body', with: 'Test answer 2'
+      attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+      click_on 'Add Answer'
+
+      expect(page).to have_link 'rails_helper.rb'
+    end
+
     scenario 'edits his answer with errors', js: true do
       sign_in user
       visit question_path(question)
