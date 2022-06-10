0242f2bb0e68243e5005ede58837408e0c3d5f81
semliko <megabytus@gmail.com>
2022-06-10 20:52:52 +0100

    chennel comments added to questions

--- app/controllers/concerns/user_comment.rb
+++ app/controllers/concerns/user_comment.rb
@@ -7,18 +7,9 @@ module UserComment
   end
 
   def make_comment
-    binding.pry
-    votable_obj = model_klass.find(params[:id])
-    vote_type = params[:vote][:vote_type]
-    votable_obj.comments.upsert(
-      { vote_type: vote_type,
-        user_id: current_user.id,
-        votable_type: model_klass.name,
-        votable_id: votable_obj.id }, unique_by: %i[votable_id votable_type user_id]
-    )
-    respond_to do |format|
-      format.json { render json: votable_obj.votes_count }
-    end
+    commentable_obj = model_klass.find(params[:id])
+    @comment = commentable_obj.comments.new(comment_params)
+    @comment.save
   end
 
   private
@@ -31,8 +22,8 @@ module UserComment
                                  )
   end
 
-  def vote_params
-    params.require(:comment).permit(:id, :body, :comment)
+  def comment_params
+    params.require(:comment).permit(:body)
   end
 
   def model_klass
