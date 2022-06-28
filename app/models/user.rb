40a8caa090ca2060e6f0a03025f35c3f6be9eb08
semliko <megabytus@gmail.com>
2022-06-28 08:42:05 +0100

    refactor find_for_oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,21 @@ class User < ApplicationRecord
 
   def self.find_for_oauth(auth)
     authorisation = Authorization.where(provider: auth.provider, uid: auth.uid.to_s).first
-    authorisation.user if authorisation
+    return authorisation.user if authorisation
+
+    email = auth.info[:email]
+    user = User.where(email: email).first
+    if user
+      user.create_authorization(auth)
+    else
+      password = Devise.friendly_token[0, 20]
+      user = User.create(email: email, password: password, password_confirmation: password)
+      user.create_authorization(auth)
+    end
+    user
+  end
+
+  def create_authorization(auth)
+    authorizations.create(provider: auth.provider, uid: auth.uid)
   end
 end
