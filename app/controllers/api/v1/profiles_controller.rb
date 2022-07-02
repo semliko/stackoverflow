d25cd4035532fc8758a72104cc7960cd55942372
semliko <megabytus@gmail.com>
2022-07-02 19:33:24 +0100

    profiles index

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::ProfilesController < Api::V1::BaseController
   def me
-    head current_resource_owner
+    render json: current_resource_owner
+  end
+
+  def index
+    render json: User.all
   end
 end
--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -40,7 +40,34 @@ describe 'Profiles API', type: :request do
         json = JSON.parse(response.body)
 
         %w[password encrypted_password].each do |attr|
-          expect(json[attr]).to_not have_ke(attr)
+          expect(json).to_not have_key(attr)
+        end
+      end
+    end
+  end
+
+  describe 'GET /api/v1/profiles' do
+    context 'unauthorized' do
+      it 'returns 401 status if there is no access_token' do
+        get '/api/v1/profiles', headers: headers
+        expect(response.status).to eq 401
+      end
+
+      context 'authorised' do
+        let(:users) { create_list(:user, 3) }
+        let(:access_token) { create(:access_token, resource_owner_id: users.first.id) }
+
+        before do
+          get '/api/v1/profiles', params: { access_token: access_token.token }, headers: headers
+        end
+
+        it 'returns 200 status' do
+          expect(response).to be_successful
+        end
+
+        it 'returns list of profiles' do
+          json = JSON.parse(response.body)
+          expect(json.size).to eq 3
         end
       end
     end
