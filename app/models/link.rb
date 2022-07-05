97e4929b848742b47dc810b6247e1877e0890750
semliko <megabytus@gmail.com>
2022-07-05 09:24:40 +0100

    some changes

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,10 +2,10 @@ class Link < ApplicationRecord
   belongs_to :linkable, polymorphic: true
 
   validates :name, :url, presence: true
-  validates_format_of :url, with: URI::regexp(%w(http https))
+  validates_format_of :url, with: URI::DEFAULT_PARSER.make_regexp(%w[http https])
 
   def is_gist_url?
-    url.to_s =~ /https\:\/\/gist.github.com/
+    url.to_s =~ %r{https://gist.github.com}
   end
 
   def js_url?
